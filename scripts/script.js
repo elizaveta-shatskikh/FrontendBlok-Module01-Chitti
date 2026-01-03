10762c4aad41a3b9357ebceefa9fb4a32bb99f4a
Elizaveta <josephine.rus@gmail.com>
2026-01-03 23:34:06 +0300

    add faq

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function () { 
+   const header = document.querySelector('.header'); 
+   if (window.scrollY > 200) { 
+      header.classList.add('scroll'); 
+   } 
+   else { 
+      header.classList.remove('scroll'); 
+   } 
+});
