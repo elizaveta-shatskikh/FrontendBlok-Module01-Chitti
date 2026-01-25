bb2e6b39da57c9c8d87cc8d9ed62ab3d4ec4aba2
Elizaveta <josephine.rus@gmail.com>
2026-01-26 00:07:12 +0300

    add btn-up

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Smooth
+
 window.addEventListener('scroll', function () { 
    const header = document.querySelector('.header'); 
    if (window.scrollY > 200) { 
@@ -7,6 +9,8 @@ window.addEventListener('scroll', function () {
       header.classList.remove('scroll'); 
    } 
 });
+
+// Burger Menu
 
 const btnBurger = document.getElementById("btnBurger");
 const modal = document.getElementById("modal");
@@ -19,3 +23,30 @@ btnBurger.addEventListener("click", function () {
 btnClose.addEventListener("click", function () {
    modal.classList.remove("active");
 });
+
+// Button Up
+
+window.onscroll = function(){scrollFunction()};
+
+const upbuttons = document.getElementById("btnUp");
+
+for (const upbutton of upbuttons) {
+   upbutton.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+e.preventDefault();
+const href = this.getAttribute("href");
+
+document.querySelector(href).scrollIntoView({
+   behavior: "smooth"
+});
+}
+
+function scrollFunction(){
+   if ( document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+      document.getElementById('btnUp').className = 'btn-up visible';
+   } else {
+      document.getElementById('btnUp').className = 'btn-up hidden';
+   }
+}
